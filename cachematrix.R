6f5148838c6d8405926d6e60d9a70e45586f9898
Ashna Sethi <sethiash@sas.upenn.edu>
2020-06-06 00:29:18 +0530

    This is for the coursera assignment under R programming, week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makecachematrix creates a matrix object than can cache its inverse
+## makecachematrix essentially creates and returns a list of functions 
 
-## Write a short comment describing this function
+## This can be used by the Cachesolve function to either set or get the inverse of the matrix in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## initialise value to NULL
+  ## make sure cached value is stored
+  m_cache <- NULL
+ ## create matrix in working environment
+   set <- function(y) {
+    x <<- y
+    m_cache <<- NULL
+   }
+   ## get the value of the matrix 
+   get <- function() x
+   ## invert the matrix and store it in cache
+   setinverse<- function(inverse) m_cache <<- inverse
+   ##obtain the inverted matrix from cache 
+   getinverse <- function() m_cache
+   
+## return functions to working environment 
+   list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+##cacheSolve returns the inverse of the matrix created in cachematrix
+## if the matrix does not exist in cache it's created in the working environment
+## It's inverse is also created and then stored in cache 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m_cache <- x$getinverse() ## attempt to get inverse stored in cache
+  if(!is.null(m_cache)) {
+    message("getting cached data")
+    return(m_cache)
+  }
+  data <- x$get() ##create matrix since/if it doesn't exist
+  m_cache <- solve(data, ...)
+  x$setinverse(m_cache)
+  m_cache
 }
+
